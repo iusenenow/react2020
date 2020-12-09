c29795dd956cff8c37645408c21effbc0173ca39
Wei Zhang <iusenenow@gmail.com>
2020-12-10 09:36:03 +1100

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
+import { Component } from 'react'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>Hello World!</h1>
+      <OMG />
+      <ABC />
     </div>
   );
+}
+
+class OMG extends Component {
+  state = {
+    name: 'Summer',
+    age: 30
+  }
+  render() {
+    return (
+      <>
+        <h1>OMG OMG!!</h1>
+        <h2>{Math.floor(Math.random() * 10) + 1}</h2>
+        <h3>我的名字叫：{this.state.name}，我今年{this.state.age}岁。</h3>
+      </>
+    )
+  }
+}
+
+class ABC extends Component {
+  render() {
+    return <h2>ABC ABC!</h2>
+  }
 }
 
 export default App;
