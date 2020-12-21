f927618d80c4f83ab678c04aac2baf77d92a5f14
Wei Zhang <iusenenow@gmail.com>
2020-12-21 23:01:45 +1100

    update

--- src/react2021/07_组件的三大属性之props/2_对props进行限制.js
+++ src/react2021/07_组件的三大属性之props/2_对props进行限制.js
@@ -2,18 +2,25 @@ import React, { Component } from 'react'
 import { PropTypes } from 'prop-types';
 
 class Person extends Component {
+
+  // constructor(props){
+  //   //构造器是否接受props，是否传递给super，取决于：是否希望在构造器中通过this访问props，实际工作中极其罕见，基本不写constructor
+  //   super(props)
+  //   console.log('constructor', this.props);
+  // }
+
   //对标签属性进行类型、必要性的限制
-static propTypes = {
-  name: PropTypes.string.isRequired,
-  sex: PropTypes.string,
-  age: PropTypes.number,
-  speak: PropTypes.func
-}
-//指定默认标签属性值
-static defaultProps = {
-  sex: "不男不女",
-  age: 18
-}
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    sex: PropTypes.string,
+    age: PropTypes.number,
+    speak: PropTypes.func
+  }
+  //指定默认标签属性值
+  static defaultProps = {
+    sex: "不男不女",
+    age: 18
+  }
   render() {
     const { name, age, sex } = this.props
     //props是只读的
