863a8de2a47c638fa1c59539881a645f800a7d4b
Wei Zhang <iusenenow@gmail.com>
2021-01-03 13:24:59 +1100

    update

--- src/todolist-2021/App.js
+++ src/todolist-2021/App.js
@@ -10,16 +10,19 @@ export class App extends Component {
     todos: [
       { id: '001', name: '买牛奶', isDone: false },
       { id: '002', name: '睡觉', isDone: true },
-      { id: '003', name: '打代码', isDone: false }
+      { id: '003', name: '打代码', isDone: false },
+      { id: '004', name: '逛街', isDone: false },
     ]
   }
+
+  addTodo = todo => { this.setState({ todos: [todo, ...this.state.todos] }) }
 
   render() {
     const { todos } = this.state
     return (
       <div className="todo-container">
         <div className="todo-wrap">
-          <Header />
+          <Header addTodo={this.addTodo} />
           <List todos={todos} />
           <Footer />
         </div>
--- src/todolist-2021/Header/index.jsx
+++ src/todolist-2021/Header/index.jsx
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
+import { nanoid } from 'nanoid'
 
 export default class Header extends Component {
+
+  handleKeyUp = e => {
+    //解构赋值获取KeyCode，target
+    const { keyCode, target } = e
+    //判断是否为回车按键
+    if (keyCode !== 13) return
+    //添加的tudo名字不能为空
+    if (!target.value.trim()) {
+      alert('输入不能为空！')
+      return
+    }
+    //准备好一个todo对象
+    const todo = { id: nanoid(), name: target.value.trim(), isDone: false }
+    //通过addTodo回调函数将todo传递给App
+    this.props.addTodo(todo)
+    //清空输入
+    target.value = ''
+  }
+
   render() {
     return (
       <div className="todo-header">
-        <input type="text" placeholder="请输入你的任务名称，按回车键确认" />
+        <input onKeyUp={this.handleKeyUp} type="text" placeholder="请输入你的任务名称，按回车键确认" />
       </div>
     )
   }
