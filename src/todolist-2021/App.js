a34a8056b814460f1be811567ecc63c4517326b9
Wei Zhang <iusenenow@gmail.com>
2021-01-03 11:37:06 +1100

    update

--- src/todolist-2021/App.js
+++ src/todolist-2021/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import "./App.css"
+import Header from './Header/index';
+import List from './List/index';
+import Footer from './Footer/index';
 
 export class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello from App</h1>
+      <div className="todo-container">
+        <div className="todo-wrap">
+          <Header />
+          <List />
+          <Footer />
+        </div>
       </div>
     )
   }
--- src/todolist-2021/List/index.jsx
+++ src/todolist-2021/List/index.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import Item from '../Item/index';
 
 export default class List extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <ul className="todo-main">
+        <Item />
+      </ul>
     )
   }
 }
