04b95cf6e3f653fecfffc182fabf9cb92ad40d43
Wei Zhang <iusenenow@gmail.com>
2021-01-04 13:24:56 +1100

    update

--- src/todolist-2021/Item/index.jsx
+++ src/todolist-2021/Item/index.jsx
@@ -1,15 +1,25 @@
 import React, { Component } from 'react'
 
 export default class Item extends Component {
+
+  state = { mouse: false }
+
+  handleMouse = flag => {
+    return () => {
+      this.setState({ mouse: flag })
+    }
+  }
+
   render() {
     const { id, name, isDone } = this.props
+    const { mouse } = this.state
     return (
-      <li>
-        <label>
+      <li onMouseLeave={this.handleMouse(false)} onMouseEnter={this.handleMouse(true)} style={{ backgroundColor: mouse ? '#ddd' : '#fff' }}>
+        <label >
           <input type="checkbox" defaultChecked={isDone} />
           <span>{name}</span>
         </label>
-        <button className="btn btn-danger" style={{ display: "none" }}>删除</button>
+        <button className="btn btn-danger" style={{ display: mouse ? 'block' : 'none' }}>删除</button>
       </li>
     )
   }
