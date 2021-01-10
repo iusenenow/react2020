64d0553866f85785eb2397613f3d5f4abf531fe0
Wei Zhang <iusenenow@gmail.com>
2021-01-11 00:20:26 +1100

    update

--- src/reactRouter2021/App.js
+++ src/reactRouter2021/App.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react'
+import { Link, BrowserRouter as Router, Route } from 'react-router-dom'
+import About from './About';
+import Home from './Home';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello</h1>
-      </div>
+      <Router>
+        <div>
+          <div className="row">
+            <div className="col-xs-offset-2 col-xs-8">
+              <div className="page-header"><h2>React Router Demo</h2></div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-xs-2 col-xs-offset-2">
+              <div className="list-group">
+                {/* 原生html中，靠<a>跳转不同的页面 */}
+                {/* <a className="list-group-item active" href="./about.html">About</a>
+              <a className="list-group-item" href="./home.html">Home</a> */}
+                {/* 在React中靠路由连接实现切换组件 */}
+                <Link className="list-group-item" to="/about">About</Link>
+                <Link className="list-group-item" to="/home">Home</Link>
+              </div>
+            </div>
+            <div className="col-xs-6">
+              <div className="panel">
+                <div className="panel-body">
+                  <Route path="/about" component={About} />
+                  <Route path="/home" component={Home} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Router>
     )
   }
 }
