bf6504b37628173bc9c0e0a3cc98ab5ae50c4a82
Wei Zhang <iusenenow@gmail.com>
2021-01-12 11:31:26 +1100

    update

--- src/reactRouter2021/App.js
+++ src/reactRouter2021/App.js
@@ -1,40 +1,44 @@
 import React, { Component } from 'react'
-import { Link, BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
 import About from './About';
 import Home from './Home';
 import Header from './Header';
+import MyNavLink from './MyNavLink';
 
 export default class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <div className="row">
-            <div className="col-xs-offset-2 col-xs-8">
-              <Header />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-xs-2 col-xs-offset-2">
-              <div className="list-group">
-                {/* 原生html中，靠<a>跳转不同的页面 */}
-                {/* <a className="list-group-item active" href="./about.html">About</a>
-              <a className="list-group-item" href="./home.html">Home</a> */}
-                {/* 在React中靠路由连接实现切换组件 */}
-                <Link className="list-group-item" to="/about">About</Link>
-                <Link className="list-group-item" to="/home">Home</Link>
+        <Switch>
+          <div>
+            <div className="row">
+              <div className="col-xs-offset-2 col-xs-8">
+                <Header />
               </div>
             </div>
-            <div className="col-xs-6">
-              <div className="panel">
-                <div className="panel-body">
-                  <Route path="/about" component={About} />
-                  <Route path="/home" component={Home} />
+            <div className="row">
+              <div className="col-xs-2 col-xs-offset-2">
+                <div className="list-group">
+                  {/* 原生html中，靠<a>跳转不同的页面 */}
+                  {/* <a className="list-group-item active" href="./about.html">About</a>
+              <a className="list-group-item" href="./home.html">Home</a> */}
+                  {/* 在React中靠路由连接实现切换组件 */}
+                  <MyNavLink to="/about">About</MyNavLink>
+                  <MyNavLink to="/home" >Home</MyNavLink>
+                </div>
+              </div>
+              <div className="col-xs-6">
+                <div className="panel">
+                  <div className="panel-body">
+                    <Route path="/about" component={About} />
+                    <Route path="/home" component={Home} />
+                    <Redirect to="/home" />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        </Switch>
       </Router>
     )
   }
